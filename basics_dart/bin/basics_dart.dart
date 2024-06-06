8d4ee0de1c14e96d6b3f35b7fa905336cd97d143
Ashish5624 <ashishv2022@outlook.com>
2024-06-07 00:33:49 +0530

    1st commit

---
+++ basics_dart/bin/basics_dart.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+  print('Enter 2 numbers:\n');
+  var input1 = stdin.readLineSync();
+  var input2 = stdin.readLineSync();
+  print("The result is shown below:");
+  //var k = stdin.readLineSync();
+
+  var num1 = int.parse(input1!);
+  var num2 = int.parse(input2!);
+  print('Sum is = ${num1 + num2}');
+}
